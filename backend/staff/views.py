12489c0a56386726d7777cd6d10164a263b62c05
Ayush Sharma <ayu8103529974@gmail.com>
2026-04-19 19:56:24 +0530

    Remove the files

--- backend/staff/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets
-from .models import Staff
-from .serializers import StaffSerializer
-from rest_framework.permissions import IsAuthenticated
-
-class StaffViewSet(viewsets.ModelViewSet):
-    queryset = Staff.objects.all()
-    serializer_class = StaffSerializer
-    permission_classes = [IsAuthenticated]
